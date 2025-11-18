86364596e783c8239ca1f4f98d346f6a33de1aed
bellkim123 <bell@cplat.io>
2025-11-18 18:57:20 +0900

    chore: add JetBrains GitToolBox config and update formatting in database.py

--- App/database.py
+++ App/database.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import os
 
+
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker, Session
